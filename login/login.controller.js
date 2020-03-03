ed07c3a5d1f03c0bd44bcfe782a143ff5062ed27
jestrada@sasa-mexico.com <yosoyjorgeenjambre>
2020-03-02 21:56:41 -0600

    commit inicial

---
+++ login/login.controller.js
@@ -0,0 +1,45 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('LoginController', LoginController);
+
+    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
+    function LoginController($location, AuthenticationService, FlashService) {
+
+        var vm = this;
+
+        vm.login = login;
+        vm.banderaMuestraAut =true;
+        (function initController() {
+            // reset login status
+            AuthenticationService.ClearCredentials();
+        })();
+
+        function login() {
+            debugger
+            vm.dataLoading = true;
+
+            var objtmp= {
+
+                username:vm.username,
+                password:vm.password
+
+
+            };
+            AuthenticationService.Login(objtmp, function (response) {
+                if (response.success) {
+                    AuthenticationService.SetCredentials(vm.username, vm.password);
+                    $location.path('/');
+                } else {
+                    FlashService.Error(response.message);
+                    vm.dataLoading = false;
+                }
+            });
+        }
+
+
+    }
+
+})();
